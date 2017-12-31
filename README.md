5dbb3bd8b623aea440323e899b0c71d938fcb24f
jinlei01 <jinlei01@baidu.com>
2017-12-31 12:55:12 +0800

    博客草稿版提交

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Hello 
-I'm thirtyjin
- test
+# THIRTYJIN BLOG
---
+++ src/assets/js/main.js
@@ -0,0 +1,12 @@
+
+
+$('#wise-view [data-toggle="tooltip"]').tooltip({
+    animated: 'fade',
+    placement: 'bottom',
+    html: true
+});
+
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
