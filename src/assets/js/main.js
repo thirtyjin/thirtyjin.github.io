35c5a4fad4414228d6cda2dfcc58228893254127
jinlei01 <jinlei01@baidu.com>
2018-02-25 09:36:15 +0800

    about 2018

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,12 +1,18 @@
 
+( function( $ ) {
+$(document).ready(function(){
+	$("#openSideNav").click(function(){
+	  $("body").addClass("blur-on");
+		$("body").removeClass("blur-off");
+		$("#myModal").css({"display":"block"});
+		$(".sidenav").css({"left":"0px","visibility":"visible","transform":"translateX(0px)"});
+	});
 
-$('#wise-view [data-toggle="tooltip"]').tooltip({
-    animated: 'fade',
-    placement: 'bottom',
-    html: true
+	$(".closebtn, .blur_modal").click(function(){
+	  $("body").addClass("blur-off");
+		$("body").removeClass("blur-on");
+		$("#myModal").css({"display":"none"})
+		$(".sidenav").css({"left":"-250px","visibility":"hidden","transform":"translateX(-250px)"});
+	});
 });
-
-
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+} )( jQuery );
